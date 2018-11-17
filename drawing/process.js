0abf0ed5ded2da328fc1fe5fa54baae0d563f4c6
Santosh Joseph <sjoseph@groupon.com>
2018-11-16 22:51:58 -0800

    wip

--- drawing/process.js
+++ drawing/process.js
@@ -1,21 +1,19 @@
 const encoderDecoder = require('../encoding-decoding');
 
-const DEBUG = 0;
-let penUp = 0;
-let penPosition = { x: 0, y: 0 };
-let penColor = { r: 0, g: 0, b: 0, a: 0 };
-let penOutside = false;
-
-
 function processOpcodes(opcodes) {
+  const DEBUG = 0;
+  let penUp = 0;
+  let penPosition = { x: 0, y: 0 };
+  let penColor = { r: 0, g: 0, b: 0, a: 0 };
+  let penOutside = false;
   let start = 0;
   const OPCODE_LEN = 2;
   const ARG_LEN = 4;
   let output = '';
+
   try {
     while (true) {
       const opcode = opcodes.substr(start, OPCODE_LEN);
-      if (DEBUG) console.log('--OPCODE:', opcode);
       if (!opcode) {
         break;
       }
@@ -24,57 +22,38 @@ function processOpcodes(opcodes) {
         penPosition = { x: 0, y: 0 };
         penColor = { r: 0, g: 0, b: 0, a: 255 };
         start += OPCODE_LEN;
-        if (DEBUG) {
-          console.log(opcodes.substr(start));
-          console.log('CLR;');
-        }
         output += 'CLR;\n';
       } else if (opcode === '80') { //pen up or down
         start += OPCODE_LEN;
-        if (DEBUG) console.log(opcodes.substr(start));
         penUp = encoderDecoder.decode(opcodes.substr(start, 4));
         start += ARG_LEN;
-        if (DEBUG) console.log(opcodes.substr(start));
-        if (DEBUG) console.log(`PEN ${penUp === 0 ? 'UP' : 'DOWN'};`);
         output += `PEN ${penUp === 0 ? 'UP' : 'DOWN'};\n`;
       } else if (opcode === 'A0') { //set color
         start += OPCODE_LEN;
-        if (DEBUG) console.log(opcodes.substr(start));
         const r = encoderDecoder.decode(opcodes.substr(start, 4));
         start += ARG_LEN;
-        if (DEBUG) console.log(opcodes.substr(start));
         const g = encoderDecoder.decode(opcodes.substr(start, 4));
         start += ARG_LEN;
-        if (DEBUG) console.log(opcodes.substr(start));
         const b = encoderDecoder.decode(opcodes.substr(start, 4));
         start += ARG_LEN;
-        if (DEBUG) console.log(opcodes.substr(start));
         const a = encoderDecoder.decode(opcodes.substr(start, 4));
         start += ARG_LEN;
-        if (DEBUG) console.log(opcodes.substr(start));
         penColor = { r, g, b, a };
-        if (DEBUG) console.log(`CO ${r} ${g} ${b} ${a};`);
         output += `CO ${r} ${g} ${b} ${a};\n`;
       } else if (opcode === 'C0') { //Move pen
         let mvStr = 'MV ';
         start += OPCODE_LEN;
-        if (DEBUG) console.log(opcodes.substr(start));
         let x = encoderDecoder.decode(opcodes.substr(start, 4));
         start += ARG_LEN;
-        if (DEBUG) console.log(opcodes.substr(start));
         let y = encoderDecoder.decode(opcodes.substr(start, 4));
         start += ARG_LEN;
-        if (DEBUG) console.log(opcodes.substr(start));
         penPosition.x += x;
         penPosition.y += y;
         mvStr += `(${penPosition.x}, ${penPosition.y})`;
         if (isOutside(penPosition)) {
-          if (DEBUG) console.log(`${mvStr};`);
-          if (DEBUG) console.log('PEN UP');
           output += `${mvStr};\n`;
           output += 'PEN UP;\n';
           penOutside = true;
-          // console.log('--outside');
         }
         while (true) {
           let possibleOpcode = opcodes.substr(start, OPCODE_LEN);
@@ -83,22 +62,24 @@ function processOpcodes(opcodes) {
           }
           x = encoderDecoder.decode(opcodes.substr(start, 4));
           start += ARG_LEN;
-          if (DEBUG) console.log(opcodes.substr(start));
           y = encoderDecoder.decode(opcodes.substr(start, 4));
           start += ARG_LEN;
-          if (DEBUG) console.log(opcodes.substr(start));
           penPosition.x += x;
           penPosition.y += y;
           mvStr += ` (${penPosition.x}, ${penPosition.y})`;
-          if (!isOutside(penPosition)) {
-            if (DEBUG) console.log(`${mvStr};`);
-            if (DEBUG) console.log('PEN DOWN');
+          if(!penOutside && isOutside(penPosition)) {
+            output += `${mvStr};\n`;
+            output += 'PEN UP;\n';
+            penOutside = true;
+            mvStr = 'MV ';
+          }
+          else if (penOutside && !isOutside(penPosition)) {
             output += `${mvStr};\n`;
             output += 'PEN DOWN;\n';
             penOutside = false;
+            mvStr = 'MV ';
           }
         }
-        if (DEBUG) console.log(`${mvStr};`);
         output += `${mvStr};\n`;
       } else {
         console.log('ERROR: unrecognized opcode:', opcode);
@@ -128,40 +109,3 @@ function isOutside(position) {
     position.y < -8192;
 }
 
-/*
-let pen = {
-  up: 0,
-  position: { x: 0, y: 0 },
-  color: { r: 0, g: 0, b: 0, a: 0 },
-  start: 0,
-};
-
-function processClear(pen, opcodes) {
-  pen.up = 0;
-  pen.position = {x: 0, y: 0};
-  pen.color = {r: 0, g: 0, b: 0, a: 255};
-}
-
-function processColor(pen, opcodes) {
-  pen.start += OPCODE_LEN;
-  const r = encoderDecoder.decode(opcodes.substr(pen.start, 4));
-  pen.start += ARG_LEN;
-  const g = encoderDecoder.decode(opcodes.substr(pen.start, 4));
-  pen.start += ARG_LEN;
-  const b = encoderDecoder.decode(opcodes.substr(pen.start, 4));
-  pen.start += ARG_LEN;
-  const a = encoderDecoder.decode(opcodes.substr(pen.start, 4));
-  pen.start += ARG_LEN;
-  pen.color = {r, g, b, a};
-}
-
-function processMoves(pen, opcodes) {
-
-}
-
-function processPen(pen, opcodes) {
-  pen.start += OPCODE_LEN;
-  pen.up = encoderDecoder.decode(opcodes.substr(pen.start, 4));
-  pen.start += ARG_LEN;
-}
-*/
--- drawing/process.test.js
+++ drawing/process.test.js
@@ -2,5 +2,17 @@ const assert = require('assert');
 const SUT = require('./process');
 
 let output = '';
-output = SUT('F0A04000417F4000417FC040004000804001C05F205F20804000');
+//green line
+// output = SUT('F0A04000417F4000417FC040004000804001C05F205F20804000');
+// console.log(output);
+
+// output = SUT('F0A040004000417F417FC040004000804000C04000400040004000804000');
+// console.log(output);
+
+// multiple moves
+// output = SUT('F0A040004000417F417F804001C0400040004000400040004000804000');
+// console.log(output);
+
+//multiple moves (outside then inside)
+output = SUT('F0A040004000417F417F804001C08000800090009000300030001000100000000000804000');
 console.log(output);
